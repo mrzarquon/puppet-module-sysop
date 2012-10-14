a218357645977a4ed46ceab3b269ebd024a1f50d
Chris Barker <cbarker@puppetlabs.com>
2012-10-13 21:26:56 -0700

    adding files

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+#basic demonstration of using hiera data with sudo/ssh/motd/ntp modules
+#
+#
+
+class sysop {
+  include motd
+  include sudo
+  include ssh
+  include ntp
+
+
+  sudo::conf { 'operations':
+    priority => 10,
+    content  => "%sysops ALL=(ALL)\n"
+  }
+
+  motd { 'defaultmotd':
+
+
+  }
+
+
+
+
+
+}
+
+
+
+
