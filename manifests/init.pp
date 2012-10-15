587014c5de980adba94ca4741db7075df896c0e1
Chris Barker <cbarker@puppetlabs.com>
2012-10-15 15:00:26 -0700

    Fixes user creation issues along with groups
    Sudoers.d files are created properly
    
    removes ntp and motd includes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,54 @@
 #basic demonstration of using hiera data with sudo/ssh/motd/ntp modules
-#
-#
 
 class sysop {
-  include ntp
-
+  include sudo
 
   sudo::conf { 'operations':
+    priority => 20,
+    content  => "%sysops ALL=(ALL) ALL\n"
+  }
+
+  sudo::conf { 'sudoers_group':
     priority => 10,
-    content  => "%sysops ALL=(ALL)\n"
+    content  => "%sudo ALL=(ALL) ALL\n"
+  }
+
+  group { [ 'sysops', 'bob', 'anne' ]:
+    ensure => 'present',
   }
 
   user { 'bob':
     ensure   => 'present',
-    password => '$1$3FWJFDq4$ZsOUBYyydVPByu6aHznKS0',
-    home     => '/home/bob'
-    groups   => [ 'bob', 'sysops' ],
+    password => '$6$oMHSukkR$RJzBNZu10V04/m05q/aER4qco5JBZAJszTRMeReiSPz0xjA.YFaJC6UTsBcMpl3QqqblO7xro8dChLkGL5lDs0',
+    gid      => "bob",
+    home     => "/home/bob",
+    groups   => [ 'sysops' ],
     shell    => '/bin/bash',
-  }
-  user { 'cat':
-    ensure   => 'present',
-    password => '$1$3FWJFDq4$ZsOUBYyydVPByu6aHznKS0',
-    home     => '/home/cat'
-    groups   => [ 'cat', 'sysops' ],
-    shell    => '/bin/bash',
+    before   => File["/home/bob"],
   }
   
+  user { 'anne':
+    ensure   => 'present',
+    password => '$6$oMHSukkR$RJzBNZu10V04/m05q/aER4qco5JBZAJszTRMeReiSPz0xjA.YFaJC6UTsBcMpl3QqqblO7xro8dChLkGL5lDs0',
+    gid      => "anne",
+    home     => "/home/anne",
+    groups   => [ 'sysops' ],
+    shell    => '/bin/bash',
+    before   => File["/home/anne"],
+  }
+
+  file { '/home/anne':
+    ensure => 'directory',
+    mode   => '0660',
+    owner  => 'anne',
+    group  => 'anne',
+  }
+
+  file { '/home/bob':
+    ensure => 'directory',
+    mode   => '0660',
+    owner  => 'bob',
+    group  => 'bob',
+  }
+
 }
-
-
-
-
